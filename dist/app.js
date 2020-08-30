d625da1c58e1c6499e60a75f08b006489225dae0
Flavio Alessandri <flavio.alessandri@gmail.com>
2020-08-30 22:40:55 +0200

    Painter Sass - Main - Second Section

--- dist/app.js
+++ dist/app.js
@@ -10979,8 +10979,51 @@ return jQuery;
 
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
+function openNavbarOnClick() {
+  var btn = $('#navBtn');
+  btn.click(function () {
+    console.log("Click");
+    $('#navbar').toggleClass('toggle');
+  });
+}
+
+function openDropDownMenuOnClick() {
+  var list = $('#navbar-top li.parent-dropdown');
+  list.on("click", function (ev) {
+    var elem = ev.target;
+    console.log("elemento cliccato", elem);
+    $(elem).next('ul').toggleClass("collapsed");
+    $(elem).next('ul').toggleClass("bg-grey"); // elem.addClass('casaas');
+  });
+}
+
+function openDropDownSubMenuOnClick() {
+  var lista = $('#navbar-top li.dropdown-submenu');
+  lista.on("click", function (ev) {
+    event.stopPropagation();
+    var elem = ev.target;
+    console.log("subelemento cliccato", elem);
+    $(elem).next('ul').toggleClass("collapsed"); // elem.addClass('casaas');
+  });
+}
+
+function changeNavbarPosition() {
+  $(window).on("scroll", function () {
+    if (window.pageYOffset > 560) {
+      $("#product-nav").addClass("stick");
+    } else {
+      $("#product-nav").removeClass("stick");
+    }
+  });
+}
+
 function init() {
+  // window.onscroll = changePos();
+  changeNavbarPosition();
   console.log("Hello World");
+  openNavbarOnClick();
+  openDropDownMenuOnClick();
+  openDropDownSubMenuOnClick();
 }
 
 $(document).ready(init);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,57 @@
 const $ = require('jquery');
 
+function openNavbarOnClick(){
+  var btn = $('#navBtn');
+  btn.click( () => {
+    console.log("Click");
+    $('#navbar').toggleClass('toggle');
+  })
+}
+
+function openDropDownMenuOnClick(){
+  var list = $('#navbar-top li.parent-dropdown');
+  list.on("click" , (ev) => {
+    var elem = ev.target;
+    console.log("elemento cliccato",elem);
+    $(elem).next('ul').toggleClass("collapsed");
+    $(elem).next('ul').toggleClass("bg-grey");// elem.addClass('casaas');
+  })
+}
+
+
+
+function openDropDownSubMenuOnClick(){
+  var lista = $('#navbar-top li.dropdown-submenu');
+  lista.on("click" , (ev)  => {
+    event.stopPropagation();
+    var elem = ev.target;
+    console.log("subelemento cliccato",elem);
+    $(elem).next('ul').toggleClass("collapsed");// elem.addClass('casaas');
+  })
+}
+
+
+function changeNavbarPosition() {
+      $(window).on("scroll" , function(){
+        if(window.pageYOffset>560) {
+          $("#product-nav").addClass("stick");
+        } else {
+          $("#product-nav").removeClass("stick");}
+    })
+}
+
 function init(){
+
+  // window.onscroll = changePos();
+
+  changeNavbarPosition();
+
+
   console.log("Hello World");
+  openNavbarOnClick();
+  openDropDownMenuOnClick();
+  openDropDownSubMenuOnClick();
+
 }
 
 $(document).ready(init);
